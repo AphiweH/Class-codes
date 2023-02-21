37ab17ba297990241bc44629fd3f45332d2aa282
AphiweH <126004704+AphiweH@users.noreply.github.com>
2023-02-21 19:22:25 +0200

    Add files via upload

--- phptemplate/index.php
+++ phptemplate/index.php
@@ -1,1 +1,19 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     
+    <title>PHP - Hello, World!</title>
+</head>
+<body>
+   
+    <h1><?php 
+	
+	
+	echo "hello world" 
+	
+	
+	?></h1>
+
+</body>
+</html>
